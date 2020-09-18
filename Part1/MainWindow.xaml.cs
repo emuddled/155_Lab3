b208b8c3cb4027e5f524f3850a855acd044869fe
ersin <ersinclair@gmail.com>
2020-09-18 09:29:24 -0700

    Add project files.

---
+++ Part1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿/// Lab No. 3		Exercise Part 1
+/// File Name: PigLatinGUI
+/// @author: Evan Sinclair
+/// Date:  Sept 18, 2020
+///
+/// Problem Statement: Use a GUI to change a name into Pig Latin and capitalize it
+/// 
+/// 
+/// Overall Plan:
+/// 1)Create text fields for input
+/// 2)Create Button for execution
+/// 3)create pigliatin names using char.ToUpper() on the second character in the name + substring third position + first letter + "ay"
+/// 4)Output pig latin names
+///
+///
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Part1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            String pigLatinFirstName;
+            String pigLatinLastName;
+            String firstName = fntxtbox.Text;
+            String lastName = lntxtbox.Text;
+            pigLatinFirstName = char.ToUpper(firstName[1]) + firstName.Substring(2) + char.ToLower(firstName[0]) + "ay";
+            pigLatinLastName = char.ToUpper(lastName[1]) + lastName.Substring(2) + char.ToLower(lastName[0]) + "ay";
+
+            MessageBox.Show("Your name in piglatin is: " + pigLatinFirstName + " " + pigLatinLastName);
+        }
+    }
+}
---
+++ Part2/Program.cs
@@ -0,0 +1,59 @@
+﻿/// Lab No. 3		Exercise Part 1
+/// File Name: LampFlowChart
+/// @author: Evan Sinclair
+/// Date:  Sept 18, 2020
+///
+/// Problem Statement: Write a program that simulates the Lamp flowchart.
+/// 
+/// 
+/// Overall Plan:
+/// 1)Create bools for possible problems with lamp
+/// 2)set bools to simulate problems
+/// 3)create "if" statements to navigate flowchart
+/// 4)output correct solution to the console
+///
+
+
+
+
+using System;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // booleans needed for navigating flowchart
+            bool pluggedIn, bulbGood;
+
+            // Comment out as need for execution
+
+            pluggedIn = false;
+            pluggedIn = true;
+
+            bulbGood = false;
+            //bulbGood = true;
+
+            // Diagram logic
+            
+            if (pluggedIn)
+            {
+                if (bulbGood)
+                {
+                    Console.WriteLine("Repair lamp");
+                }
+                else 
+                {
+                    Console.WriteLine("Replace bulb");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Plug in lamp");
+            }
+
+
+        }
+    }
+}
